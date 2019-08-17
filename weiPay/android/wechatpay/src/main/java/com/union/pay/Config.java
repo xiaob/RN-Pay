7ea911a91365bd93583cd7630ba50327f782666d
xiaob <974954753@qq.com>
2019-08-17 15:57:09 +0800

    Update Config.java

--- weiPay/android/wechatpay/src/main/java/com/union/pay/Config.java
+++ weiPay/android/wechatpay/src/main/java/com/union/pay/Config.java
@@ -7,10 +7,10 @@ package com.union.pay;
 public class Config {
 
     //微信appid
-    public static final String APP_ID = "wx874d4b2e7e874748";
+    public static final String APP_ID = "";
 
-    public static final String MCH_ID = "1497772912";
+    public static final String MCH_ID = "";
     //微信AppSecret
-    public static final String APP_SECRET= "f427a03ed7381c3fef5c638daa43682d";
-    public static final String API_KEY= "92ad0158fa6aa007e7ed28aae00c7cc1";
+    public static final String APP_SECRET= "";
+    public static final String API_KEY= "";
 }
